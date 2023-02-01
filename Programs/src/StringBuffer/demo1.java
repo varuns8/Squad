e7993d978c8da331eab262cc6bc8f6178fa832d7
luser <luser@LM21>
2023-02-01 18:51:12 +0530

    String Buffer

---
+++ Programs/src/StringBuffer/demo1.java
@@ -0,0 +1,13 @@
+package StringBuffer;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		String s=new String("deepak");
+		System.out.println(s); //deepak
+		s=s.concat("pawar");
+		System.out.println(s); //deepakpawar
+
+	}
+
+}
