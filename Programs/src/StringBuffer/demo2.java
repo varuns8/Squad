b4677706d399e3989d17c1815fd5c35a3a2bb9bf
luser <luser@LM21>
2023-02-02 17:57:04 +0530

    String buffer

---
+++ Programs/src/StringBuffer/demo2.java
@@ -0,0 +1,17 @@
+package StringBuffer;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		String s1 = new String("deepak");
+		String s2 = new String("deepak");
+		// if different case then it allocate different memory location.
+		System.out.println(s1==s2);// == it allocate the address.
+		System.out.println(s1.equals(s2)); // equals() is allocate the contain.
+		String s3 = "amit";
+		String s4 = "amit";
+		System.out.println(s3==s4);
+		
+	}
+
+}
