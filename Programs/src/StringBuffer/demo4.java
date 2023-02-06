41bff429108df7642faf963ad3e06e1082371509
luser <luser@LM21>
2023-02-06 18:05:06 +0530

    Replace()

--- Programs/src/StringBuffer/demo4.java
+++ Programs/src/StringBuffer/demo4.java
@@ -3,6 +3,9 @@ package StringBuffer;
 public class demo4 {
 
 	public static void main(String[] args) {
+		
+		// Replace () is used to replace old to new String
+		
 		String s1 = "Javed is a Developer and Javed is a good Person";
 		String s2 = s1.replace("Javed", "Rahul");
 		System.out.println(s2);
