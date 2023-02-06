4c39326c8dc73d38f1e954cdfa60de30452f2ae6
luser <luser@LM21>
2023-02-06 18:03:47 +0530

    Replace()

---
+++ Programs/src/StringBuffer/demo4.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		String s1 = "Javed is a Developer and Javed is a good Person";
+		String s2 = s1.replace("Javed", "Rahul");
+		System.out.println(s2);
+
+	}
+
+}
