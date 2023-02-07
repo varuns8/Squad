315d2034614b3ae2c476f174cd6a8524b1c0a94b
luser <luser@LM21>
2023-02-07 17:17:33 +0530

    Trim()

---
+++ Programs/src/StringBuffer/TrimMethod.java
@@ -0,0 +1,13 @@
+package StringBuffer;
+
+public class TrimMethod {
+
+	public static void main(String[] args) {
+		String s1="	What's Up!	";
+		System.out.println("Original: "+s1);
+		s1=s1.trim();
+		System.out.println("\nAfter: "+s1);
+
+	}
+
+}
