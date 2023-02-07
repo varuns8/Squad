750b3f2841a819db54ac308bebd3e8d8ea4fbd4f
luser <luser@LM21>
2023-02-07 18:02:47 +0530

    substring()

---
+++ Programs/src/StringBuffer/demo9_SubstringMethod.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class demo9_SubstringMethod {
+
+	public static void main(String[] args) {
+		String s1 = "hello world";
+		//String s2 = s1.substring(15);
+		System.out.println(s1.substring(10));
+
+	}
+
+}
