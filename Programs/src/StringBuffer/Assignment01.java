c38b89b4117a2e2058d08c02ecb28b268ea87b67
luser <luser@LM21>
2023-02-08 18:20:22 +0530

    Assignment01

---
+++ Programs/src/StringBuffer/Assignment01.java
@@ -0,0 +1,18 @@
+package StringBuffer;
+
+public class Assignment01 {
+
+	public static void main(String[] args) {
+		
+		String s = "I Love My Country";
+		String word[] = s.split(" ");
+		
+		for(int i=word.length-1; i>=0; i--) {
+			for (int j=0; j<=i;j++) {
+				System.out.print(word[j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
