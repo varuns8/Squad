846d30827cd94c51d19b843f87c5a29c646d8080
luser <luser@LM21>
2023-02-10 18:34:03 +0530

    First Letter

--- Programs/src/StringBuffer/Assignment05.java
+++ Programs/src/StringBuffer/Assignment05.java
@@ -1,14 +1,23 @@
 package StringBuffer;
 
+import java.util.Scanner;
+
 public class Assignment05 {
 
 	public static void main(String[] args) {
-		String s = "Rahul Ravindra Shinde";
-		String parts[] = s.split(" ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Here: ");
+		String s = sc.nextLine();
 		
-		for(String n:parts) {
-			System.out.print(n.charAt(0));
+		String s1 = s.toUpperCase();
+		String s2[] = s1.split(" ");
+		
+		for(int i=0; i<s2.length;i++) {
+			System.out.print(s2[i].charAt(0)+".");
 		}
+		
+		
+		
 
 	}
 
