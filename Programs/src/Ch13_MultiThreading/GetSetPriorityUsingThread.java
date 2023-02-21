fc3de6315a404824ffb286669202b841e2e8caa0
luser <luser@LM21>
2023-02-21 18:18:17 +0530

    getsetPriority with join method

--- Programs/src/Ch13_MultiThreading/GetSetPriorityUsingThread.java
+++ Programs/src/Ch13_MultiThreading/GetSetPriorityUsingThread.java
@@ -2,7 +2,7 @@ package Ch13_MultiThreading;
 
 class demo1 extends Thread{
 	public void run() {
-		Thread.currentThread().setPriority(MAX_PRIORITY);;
+		Thread.currentThread().setPriority(3);;
 		System.out.println("Child Thread: "+Thread.currentThread().getPriority());
 	}
 }
@@ -11,10 +11,21 @@ public class GetSetPriorityUsingThread {
 
 	public static void main(String[] args) {
 		demo1 d1 = new demo1();
-		Thread.currentThread().setPriority(3);
-		System.out.println("Main Thread: "+Thread.currentThread().getPriority());
 		d1.start();
-
+		
+		try {
+			d1.join();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Main Thread:
+		Thread.currentThread().setPriority(6);
+		System.out.println("Main Thread: "+Thread.currentThread().getPriority());
+		
+		
+		
 	}
 
 }
