3451c66fe172bec03cb30440d51efd5f3e0ab1bc
luser <luser@LM21>
2023-02-27 17:49:59 +0530

    Demo2 by user input

---
+++ Programs/src/Ch14_FileHandling/Demo02.java
@@ -0,0 +1,30 @@
+package Ch14_FileHandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Note Here: ");
+		String s = sc.nextLine();
+		try
+		{
+			FileOutputStream f01 = new FileOutputStream("Diary.txt");
+			byte b[]=s.getBytes();
+			f01.write(b);
+			System.out.println("Data has been written");
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+}
