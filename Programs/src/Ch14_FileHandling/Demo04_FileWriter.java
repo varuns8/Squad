44b7de639c253b2f7f954310dbb1dc809591852b
luser <luser@LM21>
2023-03-01 17:08:45 +0530

    file reader and writer

---
+++ Programs/src/Ch14_FileHandling/Demo04_FileWriter.java
@@ -0,0 +1,27 @@
+package Ch14_FileHandling;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Demo04_FileWriter {
+
+	public static void main(String[] args) {
+		try
+		{
+			FileWriter fw = new FileWriter("fileinfo.txt",true);//here true is used to continue write.
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Here: ");
+			String s1 = sc.nextLine();
+			fw.write(s1);
+			fw.flush(); // to cleaning previous data
+			fw.close(); // to close previous resources
+			System.out.println("Data has been written!");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ Programs/src/Ch14_FileHandling/Demo05_FileReader.java
@@ -0,0 +1,27 @@
+package Ch14_FileHandling;
+
+import java.io.FileReader;
+
+public class Demo05_FileReader {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+			FileReader fr= new FileReader("fileinfo.txt");
+			char ch[] = new char[100];
+			fr.read(ch);
+			for(char c:ch) //here in c each and every single character read of ch and store it as a string
+			{
+				System.out.print(c);
+			}
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
