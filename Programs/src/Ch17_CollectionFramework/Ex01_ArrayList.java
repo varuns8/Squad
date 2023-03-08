56311e1e6ff49afef2a16ceeae6d311335e6859b
luser <luser@LM21>
2023-03-08 18:53:31 +0530

    commit

---
+++ Programs/src/Ch17_CollectionFramework/Ex01_ArrayList.java
@@ -0,0 +1,18 @@
+package Ch17_CollectionFramework;
+import java.util.ArrayList;
+
+public class Ex01_ArrayList {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("sam");
+		al.add(12);
+		al.add(2.3);
+		al.add('c');
+		al.add("sam");
+		al.add(null);
+		al.add(null);
+		System.out.println(al);
+		System.out.println(al.get(2));
+	}
+
+}
