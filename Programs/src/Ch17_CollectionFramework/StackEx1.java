1676bd8005c6b1ea32c2a207fc85092e31fe9c16
luser <luser@LM21>
2023-03-17 18:45:52 +0530

    commit

---
+++ Programs/src/Ch17_CollectionFramework/StackEx1.java
@@ -0,0 +1,29 @@
+package Ch17_CollectionFramework;
+
+import java.util.Enumeration;
+import java.util.Stack;
+
+public class StackEx1 {
+
+	public static void main(String[] args) {
+		Stack<String> s=new Stack<>();
+		s.add("Green");
+		s.push("Blue");
+		s.push(null);
+		s.push("Green");
+		s.push("Black");
+		
+		System.out.println("Before Remove : "+s);
+		s.pop();
+		System.out.println("After Remove"+s);
+		
+		System.out.println("-----------------Using Enumeration-----------------");
+		Enumeration<String> e=s.elements();
+		
+		while(e.hasMoreElements()) {
+			System.out.println(e.nextElement());
+		}
+
+	}
+
+}
